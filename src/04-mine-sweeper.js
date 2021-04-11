05465918a002de8e7d4de9608cfde86158b3e789
dimabelus <dima21belus@gmail.com>
2021-04-11 21:41:00 +0300

    feat: implement minesweeper function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,38 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
-}
 
+function minesweeper(matrix) {
+  const array = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === false) {
+        array.push(0);
+      } else if (matrix[i][j] === true) {
+        array.push(1);
+      }
+    }
+  }
+  const sweepedArray = [];
+  for (let i = 0; i < Math.ceil(array.length / 3); i++) {
+    sweepedArray[i] = array.slice(i * 3, i * 3 + 3);
+  }
+  const column = sweepedArray[0].length;
+  for (let i = 0; i < column; i++) {
+    for (let j = 0; j < sweepedArray.length; j++) {
+      if (sweepedArray[j][i] === 1 && i === 0) {
+        sweepedArray[j][i + 1] = 2;
+      } else if (sweepedArray[j][i] === 1 && i === 1) {
+        sweepedArray[j][i - 1] = 2;
+      } else if (sweepedArray[j][i] === 2) {
+        sweepedArray[j][i + 1] = 1;
+      } else if (sweepedArray[j][i - 1] === 1) {
+        sweepedArray[j][i] = 1;
+      } else if (sweepedArray[0][i] === 1) {
+        sweepedArray[j][i] = 1;
+      }
+    }
+  }
+  return sweepedArray;
+}
 module.exports = minesweeper;
