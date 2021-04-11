cae9cda22a980f46c121dee0f7ba3b16cb68ef27
dimabelus <dima21belus@gmail.com>
2021-04-11 22:39:12 +0300

    feat: implement renameFiles function

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,22 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  let array = [...names];
+  const newArr = [...names];
+  let n;
+  for (let i = 0; i < array.length; i++) {
+    array = newArr.map((item) => item);
+    n = 0;
+    for (let j = 0; j < newArr.length; j++) {
+      if (array[i] === newArr[j] && n > 0) {
+        newArr[j] = `${array[j]}(${n++})`;
+      } else if (array[i] === newArr[j] && n === 0) {
+        n++;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = renameFiles;
