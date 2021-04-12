4adb7432f50e5189708c65db35969442bb2e476d
dimabelus <dima21belus@gmail.com>
2021-04-12 16:03:19 +0300

    feat: implement encodeLine function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,19 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+
+function encodeLine(str) {
+  let x = 1;
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      x += 1;
+    } else {
+      result += x + str[i];
+      x = 1;
+    }
+  }
+  return result.replace(/1/g, '');
 }
 
 module.exports = encodeLine;
